99cb873e8c8937ef98148b92afe5d70cd8746ecb
Danny Seymour <danny.saseymour@gmail.com>
2019-12-02 18:17:27 -0500

    initial setup

--- MiddlewareTest/Program.cs
+++ MiddlewareTest/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 
 namespace MiddlewareTest
 {
@@ -14,13 +17,44 @@ namespace MiddlewareTest
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //setup config begins
+            var currentEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{currentEnv}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+            //setup config ends
+
+            //configure logger begins
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+            //configure logger ends
+
+            try
+            {
+                Log.Information("Starting web host");
+                BuildWebHost(args).Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Web Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-
+         WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>()
+               .UseSerilog()
+                .Build();
           ;
     }
 }
